eb61ac314992c57f3462c10fa2f83f912b004ce1
allanos1 <anthony.llanos@upr.edu>
2014-03-27 22:14:33 -0400

    Testing Commits

--- Implementation/ccs/Aerobal/main.c
+++ Implementation/ccs/Aerobal/main.c
@@ -1,33 +1,6 @@
 #include "a.lib/ABTest.h"
 
 int main(void){
-	////////////////////
-	// Begin Tests
 
-	ABTestLCD(ABTEST_BOOL_TRUE);
-	ABTestWindVane(ABTEST_BOOL_TRUE);
-	ABTestBMP(ABTEST_BOOL_TRUE);
-	ABTestRelay(ABTEST_BOOL_TRUE);
-	ABTestBT(ABTEST_BOOL_TRUE);
-	ABTestDHT(ABTEST_BOOL_TRUE);
-	ABTestAnalog(ABTEST_BOOL_TRUE);
-	ABTestServo(ABTEST_BOOL_TRUE);
-	ABTestButtons(ABTEST_BOOL_TRUE);
-
-	//Re-Run.
-	ABTestLCD(ABTEST_BOOL_FALSE);
-	ABTestWindVane(ABTEST_BOOL_FALSE);
-	ABTestBMP(ABTEST_BOOL_FALSE);
-	ABTestRelay(ABTEST_BOOL_FALSE);
-	ABTestBT(ABTEST_BOOL_FALSE);
-	ABTestDHT(ABTEST_BOOL_FALSE);
-	ABTestAnalog(ABTEST_BOOL_FALSE);
-
-
-	// End Tests
-	/////////////////////
-
-	lcdClear();
-	ABTestLog("ABTL: Aerobal","-------------","Aerobal System","Testing Successful!");
 	while(1);
 }
